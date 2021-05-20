54d7e859e904a5e33a43a0024130341bf415a24f
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-05-20 08:53:25 +0100

    chore: prepare docker resources and code

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("<h1>Hi there, welcome to your app</h1>");
 });
 
+app.use(express.json());
+app.post("/comment", (req, res) => {
+  res.send(req.body);
+});
+
+const ymlParser = express.raw({ type: "application/yml" });
+
+app.post("/geek-comment", ymlParser, (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`server running on port ${process.env.PORT}`)
 );
